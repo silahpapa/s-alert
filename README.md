bba5e728a6d3e0459145a5244d089b630df3126d
silah <silakosy@gmail.com>
2023-04-08 00:08:56 +0300

    add confirmRequest

--- README.md
+++ README.md
@@ -32,6 +32,25 @@ swalSuccess('The operation was successful.', 'Great job!');
 swalError('An error occurred. Please try again later.', 'Oops!');
 
 ```
+## Show Toast
+first import the showToast function:
+```angular2html
+import { showToast } from 's-alert';
+```
+Then, call the function with the message you want to display in the toast:
+
+```
+showToast('Hello, world!');
+```
+By default, the toast will be a success message that appears at the top-right corner of the screen and disappears after 3 seconds. You can customize these options by passing in additional parameters to the showToast function:
+
+```
+showToast('Something went wrong!', 'error', 'bottom-start', 5000);
+```
+
+The second parameter is the type of the toast, which can be 'warning', 'error', or 'success' (default is 'success'). The third parameter is the position of the toast, which can be 'top-start', 'top-end', 'bottom-start', 'bottom-end', or 'center' (default is 'top-end'). The fourth parameter is the time in milliseconds for the toast to disappear (default is 3000).
+
+
 
 
 
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import Swal from 'sweetalert2';
-
+import { doPost } from 's-apis'
 export const swalSuccess = (message: string,title: string): void => {
     let title_: string = title ?? 'success!';
     Swal.fire(title_, message, 'success');
@@ -10,9 +10,9 @@ export const swalError = (message: string, title: string): void => {
     Swal.fire(title_, message, 'error');
 };
 
-export const showToast = (message: string, toastType: string,toastPosition:string, toastTime:number): void => {
-    const toastPosition_ = toastPosition ?? 'top-end'
-    const toastTime_ = toastTime ?? 'top-end'
+export const showToast = (message: string, toastType?: string,toastPosition?:string, toastTime?:number): void => {
+    const toastPosition_:string = toastPosition ?? 'top-end'
+    const toastTime_:number = toastTime ?? 3000
     // @ts-ignore
     const Toast = Swal.mixin({
         toast: true,
@@ -31,4 +31,34 @@ export const showToast = (message: string, toastType: string,toastPosition:strin
         icon: toastType,
         title: message
     })
+}
+export const confirmRequest = async (url: string, message: string, title?: string, data?: any): Promise<Swal.SweetAlertResult> => {
+
+    const result = await Swal.fire({
+        title: title ?? 'Are you sure?',
+        html: message,
+        showCancelButton: true,
+        confirmButtonColor: '#32c787',
+        cancelButtonText: 'No, cancel',
+        confirmButtonText: 'Yes, Proceed!',
+        reverseButtons: true,
+        showLoaderOnConfirm: true,
+        preConfirm: async () => {
+            try {
+                const response = await doPost(url, data);
+                return {
+                    response: response.data,
+                    success: true,
+                };
+            } catch (error) {
+                return {
+                    success: false,
+                    error: error,
+                };
+            }
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+    });
+
+    return result;
 }
